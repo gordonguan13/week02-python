39e668da9d2321fc0d8ba8128237551439e226f6
Gordon <gordonguan13@gmail.com>
2026-09-04 21:26:28 -0700

    day 1

--- count_vowels.py
+++ count_vowels.py
@@ -1,6 +1,7 @@
 s = input("sentence: ")
+word = s.lower()
 counter = 0
-for i in s:
-    if i in ['a','e','i','o','u']:
+for i in word:
+    if i in ['a','e','i','o','u',]:
         counter +=1
 print("Vowels: ", counter)
